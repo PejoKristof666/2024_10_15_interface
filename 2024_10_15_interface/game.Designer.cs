78c3f19c98866c90812d7752c73473ee4b902fcf
Péjó Kristóf <pejo.kristof.21i@egressy.info>
2024-10-16 10:39:31 +0200

    KURVAAAAAA

--- 2024_10_15_interface/game.Designer.cs
+++ 2024_10_15_interface/game.Designer.cs
@@ -30,25 +30,54 @@ namespace _2024_10_15_interface
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.road = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2PictureBox3 = new Guna.UI2.WinForms.Guna2PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.road)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(148, 137);
+            this.label1.Location = new System.Drawing.Point(1027, 43);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
+            // road
+            // 
+            this.road.BorderRadius = 10;
+            this.road.ImageRotate = 0F;
+            this.road.Location = new System.Drawing.Point(1, 216);
+            this.road.Name = "road";
+            this.road.Size = new System.Drawing.Size(1199, 200);
+            this.road.TabIndex = 2;
+            this.road.TabStop = false;
+            this.road.Click += new System.EventHandler(this.guna2PictureBox2_Click);
+            // 
+            // guna2PictureBox3
+            // 
+            this.guna2PictureBox3.BorderRadius = 10;
+            this.guna2PictureBox3.ImageRotate = 0F;
+            this.guna2PictureBox3.Location = new System.Drawing.Point(599, -1);
+            this.guna2PictureBox3.Name = "guna2PictureBox3";
+            this.guna2PictureBox3.Size = new System.Drawing.Size(172, 601);
+            this.guna2PictureBox3.TabIndex = 3;
+            this.guna2PictureBox3.TabStop = false;
+            // 
             // game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(383, 339);
+            this.ClientSize = new System.Drawing.Size(1200, 600);
+            this.Controls.Add(this.guna2PictureBox3);
+            this.Controls.Add(this.road);
             this.Controls.Add(this.label1);
             this.Name = "game";
             this.Text = "game";
+            ((System.ComponentModel.ISupportInitialize)(this.road)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,5 +86,7 @@ namespace _2024_10_15_interface
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2PictureBox road;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox3;
     }
 }
--- 2024_10_15_interface/game.cs
+++ 2024_10_15_interface/game.cs
@@ -12,6 +12,9 @@ namespace _2024_10_15_interface
 {
     public partial class game : Form
     {
+        car c;
+        public bool hit;
+        public Timer t = new Timer();
         public user loggedInUser { get; set; }
         public game(user loggedInUser)
         {
@@ -23,6 +26,76 @@ namespace _2024_10_15_interface
         {
             label1.Text = $"Üdv. {loggedInUser.username}\nPontjaid száma: {loggedInUser.point}";
             label1.AutoSize = true;
+            
+            t.Interval = 2000;
+            t.Start();
+            t.Tick += (s, e) =>
+            {
+                label1.Hide();
+                
+            };
+            CarMake();
+            if (hit == true)
+            {
+                CarMake();
+            }
+        }
+
+        public void CarMake()
+        {
+            Timer carTimer = new Timer();
+            car c = new car();
+            
+            Controls.Add(c);
+            c.Width = 50;
+            c.Height = road.Height / 2;
+            c.Location = new Point(road.Left + c.Width, road.Top + c.Height / 2);
+            c.BringToFront();
+            c.BackColor = Color.Red;
+
+            
+            carTimer.Interval = 10;
+            carTimer.Start();
+            carTimer.Tick += (s, e) =>
+            {
+                c.Left += 2;
+                //if (c.Right < c.Left - 20)
+                //{
+                //    carTimer.Stop();
+                //}
+                //else
+                //{
+                //    carTimer.Start();
+                //}
+                if (c.Left >= (road.Right + c.Width))
+                {
+
+                    Controls.Remove(c);
+                    c.Hide();
+                    hit = true;
+                    carTimer.Stop();
+                }
+
+            };
+            
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void guna2PictureBox2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
